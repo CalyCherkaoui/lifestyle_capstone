53b47d8a003191f99d30ef30fb29f28ae2f213fd
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-14 13:00:08 -0500

    Adding associations and validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :articles
+  has_many :votes, dependent: :destroy
+
+  validates :name, uniqueness: { case_sensitive: false}, presence: true, lenght: {minimum: 6}
+
 end
