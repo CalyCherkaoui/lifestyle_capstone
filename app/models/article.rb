3536cfaf8578b3162b627730a57a2e6c4c586410
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-14 13:00:28 -0500

    adding associations to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,9 @@
 class Article < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: :user
+  belongs_to :category
+  has_many :votes, dependent: :destroy
+
+  validates :title, presence: true, lenght: { maximum: 60 }
+  validates :text, presence: true
+  validates :category_id, presence: true
 end
