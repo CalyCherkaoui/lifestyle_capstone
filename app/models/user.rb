e49b427448515057228523d6a0525654dcb0eb1b
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-14 13:24:47 -0500

    adding validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   has_many :articles
   has_many :votes, dependent: :destroy
 
-  validates :name, uniqueness: { case_sensitive: false}, presence: true, lenght: {minimum: 6}
+  validates :name, uniqueness: { case_sensitive: false }, presence: true, allow_blank: false
+  validates :name, length: { 
+    minimum: 6,
+    too_short: "must have at least %{count} characters",
+  }
 
 end
