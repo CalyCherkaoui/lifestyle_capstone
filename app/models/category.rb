d9d4e8424c3e43b8c8121040fcf4270c78e248ed
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-14 13:00:43 -0500

    adding associations to category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+  has_many :articles
+  validates :name, presence: true, uniqueness: true, case_sensitive: false
+  validates :priority, presence: true, numiracality: { greater_than: 0 }
 end
