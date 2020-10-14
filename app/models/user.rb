4e74f2f0a89058ae76e447ef8173dd26c7b580c5
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-14 13:47:37 -0500

    updating validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   validates :name, uniqueness: { case_sensitive: false }, presence: true, allow_blank: false
   validates :name, length: { 
-    minimum: 6,
+    minimum: 3,
     too_short: "must have at least %{count} characters",
   }
 
