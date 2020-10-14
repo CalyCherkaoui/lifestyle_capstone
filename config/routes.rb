72c53d739e8a87ce2ca4b63ad801cecd44012ef9
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-14 18:05:45 -0500

    configuring databasecleaner and shoulda

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   root to: 'articles#index'
 
   resources :categories
-  resources :articles do
-    resource :vote, controller => 'articles/vote'
-  end
+  resources :articles
+  # resources :articles do
+  #   resource :vote, controller => 'articles/vote'
+  # end
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:author).class_name('user')}
+    it { should belong_to(:category)}
+    it { should have_many(:votes)}
+  end
+
+  describe 'article.Title Attribute validations' do
+    it { should validate_presence_of(:title)}
+    it { should validate_length_of(:title).
+      is_at_least(6).
+      with_message("must have at least 10 characters")
+    }
+    it { should validate_length_of(:title).
+        is_at_most(60).
+        with_message("60 characters is the maximum allowed")
+    }
+  end
+
+  describe 'article.Text Attribute validation' do
+    it { should validate_presence_of(:text)}
+    it { should validate_presence_of(:text)}
+    it { should validate_length_of(:text).
+                  is_at_least(10).
+                  with_message("must have at least 10 characters")
+    }
+  end
+
+  describe 'article.Category_id Attribute validation' do
+    it { should validate_presence_of(:category_id)}
+  end
 end
