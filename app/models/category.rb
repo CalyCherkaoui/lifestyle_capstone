f8ac58f5dbbff3113a9a8b82acc35fbd9e36262f
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-14 13:24:11 -0500

    adding validations to catagory

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,11 @@
 class Category < ApplicationRecord
   has_many :articles
-  validates :name, presence: true, uniqueness: true, case_sensitive: false
-  validates :priority, presence: true, numiracality: { greater_than: 0 }
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, allow_blank: false
+  validates :name, length: { 
+    minimum: 2,
+    maximum: 20,
+    too_short: "must have at least %{count} words",
+    too_long: "%{count} characters is the maximum allowed"
+  }
+  validates :priority, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
