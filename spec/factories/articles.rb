2355fc6c5c38ad850b07ded730e6c4da2372f5e6
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-14 21:16:25 -0500

    Defining some factories

---
+++ spec/factories/articles.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :article do
+    title { Faker::JapaneseMedia::StudioGhibli.movie }
+    text { Faker::Lorem.paragraphs}
+    author_id { FactoryBot.create(:user).id }
+    category_id { FactoryBot.create(:category).id }
+  end
+end
