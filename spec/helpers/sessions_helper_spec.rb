fc838c7b70d8ed5df80c2e91ebd4d7ce992112d1
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-15 15:11:59 -0500

    Testing session helper

--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -11,5 +11,10 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe SessionsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let( :user ) { FactoryBot.create :user}
+  # @user = FactoryBot.create(:user)
+  it 'Create session with a user to be logged in' do
+    log_in(user)
+    expect(session[:user_id]).to eq( user)
+  end
 end
