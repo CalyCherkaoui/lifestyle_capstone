3a6dc1b132fbbc174963adc01b522a56e50acabc
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-15 10:38:07 -0500

    create routing for sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root to: 'articles#index'
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/login', to: 'sessions#destroy'
 
   resources :categories
   resources :articles
