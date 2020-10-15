afc31fa8b494d54e16f2037ac3835b49f61d5b3b
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-15 15:12:58 -0500

    creating categories views

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-  has_many :articles
+  has_many :articles, -> { order(created_at: :desc) }
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :name, length: {
     minimum: 2,
