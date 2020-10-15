3a8eb5e81110ccfcf28553a140da57de57ecb2a2
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-14 20:14:01 -0500

    testing user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   has_many :articles
   has_many :votes, dependent: :destroy
 
-  validates :name, uniqueness: { case_sensitive: false }, presence: true, allow_blank: false
+  validates :name, uniqueness: { case_sensitive: false }, presence: true
   validates :name, length: {
     minimum: 3,
-    too_short: 'must have at least %<count> characters'
+    # too_short: 'must have at least 3 characters'
   }
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'associations' do
+    it { should have_many(:articles) }
+    it { should have_many(:votes) }
+  end
+
+  describe 'Name validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name).case_insensitive }
+    it { should validate_length_of(:name).is_at_least(3)}
+  end
+end
