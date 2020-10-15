5b3ae4c1b25cb059b76dcdb4af2b3be8aa3d69ba
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-15 10:55:53 -0500

    Adding session create action and login view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(name: params[:session][:name])
+    if user
+      # loggig
+      redirect_to root_path, notice: 'You have logged-in successfully!'
+    else
+      flash.now[:danger] = 'Invalid User name!'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
