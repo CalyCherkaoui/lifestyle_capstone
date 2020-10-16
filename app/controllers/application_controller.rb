d2b3342cb67f974559ee25f8b2a170c9de9216fc
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-15 20:44:37 -0500

    Adding scopes of category

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+  before_action :set_important_categories
+
+  private
+  def set_important_categories
+    @categories_important = Category.importants
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -8,4 +8,16 @@ class Category < ApplicationRecord
     # too_long: "20 characters is the maximum allowed"
   }
   validates :priority, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  scope :priority_sorted, -> { order(priority: :asc) }
+  scope :importants, -> { priority_sorted.limit(4)}
+  
+  def cover_article
+    self.articles.first
+  end
+
+  # category.cover_article the mosted voted recent arcticle
+  # scope :most_voted, -> { with_votes.order(votes_count: :desc) }
+  # scope :with_votes, -> { select('articles.*, COUNT(votes.user_id) as votes_count').left_joins(:votes).group(:id) }
+  # scope :cover_article, -> { select('articles.*, ')  }
 end
