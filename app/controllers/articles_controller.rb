10c9ec26d8bd900a1a6481b4c90c0b4592ad1b02
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-17 17:53:00 -0500

    Updating article controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,10 @@ class ArticlesController < ApplicationController
 
   # GET /articles
   def index
-    @articles = Article.heros
+    @article_default = Article.first
+    @article_latest = Article.heros.first
     @hero_article = Vote.most_voted_article
+    @important_categories = Category.important_with_articles
   end
 
   # GET /articles/1
