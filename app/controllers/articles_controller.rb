b4b9a7281abcbad1c45f975297dc5b3e966892b3
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-18 15:19:32 -0500

    Add authent. confirmation before action to article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[show edit update destroy]
+  before_action :confirm_logged_in_user, only: %i[new edit update destroy]
 
   # GET /articles
   def index
