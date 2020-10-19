a9d94c715763d51558a67cc94a89032769b592c8
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-19 17:14:38 -0500

    Testing log-in feature

---
+++ spec/features/login_process_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "LoginProcesses", type: :feature do
+  scenario "User log-in to her/his account" do
+    User.create(name: 'test_person')
+    visit "/login"
+    # click_button('log-in')
+    expect(current_path).to have_content('/login')
+    fill_in('session[name]', with: 'test_person')
+    click_button('Log-in')
+    expect(current_path).to have_content('/')
+    expect(page).to have_text("You have logged-in successfully!")
+  end
+
+  scenario "User log-out from her/his account" do
+    User.create(name: 'test_person2')
+    visit "/"
+    click_link('Log-in')
+    expect(current_path).to have_content('/login')
+    fill_in('session[name]', with: 'test_person2')
+    click_button('Log-in')
+    expect(current_path).to have_content('/')
+    expect(page).to have_text("You have logged-in successfully!")
+    click_link('Log-out')
+    expect(current_path).to have_content('/')
+    expect(page).not_to have_text("Profile")
+  end
+end
