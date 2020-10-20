dd701606c691a2e3d111122d446422fee2043027
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-19 20:05:51 -0500

    Integration tests

--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe SessionsHelper, type: :helper do
-  # let(:user) { User.create(name: 'person') }
   user_logged = User.create(name: 'person')
   it 'Create session with a user to be logged in' do
     log_in(user_logged)
