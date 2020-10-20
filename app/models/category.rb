75b0109da49d9ea88206b5134a2ba8165862f73a
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-10-20 11:04:06 -0500

    Fixing broken links

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,8 +4,6 @@ class Category < ApplicationRecord
   validates :name, length: {
     minimum: 2,
     maximum: 20
-    # too_short: "must have at least 2 words",
-    # too_long: "20 characters is the maximum allowed"
   }
   validates :priority, presence: true, numericality: { only_integer: true, greater_than: 0 }
 
